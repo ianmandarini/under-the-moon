f003b1c926ad8f3c43d9df557b6f2e944a0e4fc0
Ian Mandarini <ian@ianmandarini.com>
2020-10-04 12:49:00 -0300

    time lerp

---
+++ Assets/Scripts/LoopTime/TimeLerpPosition.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace LoopTime
+{
+    public class TimeLerpPosition: MonoBehaviour
+    {
+        [SerializeField] private TimeSO timeSO = default;
+        [SerializeField] private GameObject target = default;
+        [SerializeField] private Transform a = default;
+        [SerializeField] private Transform b = default;
+
+        private void OnEnable()
+        {
+            this.timeSO.TimeTicked += this.TimeTickedEventHandler;
+        }
+
+        private void OnDisable()
+        {
+            this.timeSO.TimeTicked -= this.TimeTickedEventHandler;
+        }
+
+        private void TimeTickedEventHandler(object sender, TimeTickedEventArgs e)
+        {
+            this.target.gameObject.transform.position = Vector3.Lerp(this.a.position, this.b.position, e.ToT);
+        }
+    }
+}
