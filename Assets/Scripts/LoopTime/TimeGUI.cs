ed64ed878f855d4a81c12383e315fa6767e81415
Ian Mandarini <ian@ianmandarini.com>
2020-10-04 00:20:09 -0300

    time gui

--- Assets/Scripts/LoopTime/TimeGUI.cs
+++ Assets/Scripts/LoopTime/TimeGUI.cs
@@ -1,7 +1,28 @@
-﻿namespace LoopTime
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace LoopTime
 {
-    public class TimeGUI
+    public class TimeGUI: MonoBehaviour
     {
-        
+        [SerializeField] private TMP_Text textGUI = default;
+        [SerializeField] private TimeSO timeSO = default;
+
+        private void OnEnable()
+        {
+            this.timeSO.TimeTicked += this.TimeTickedEventHandler;
+        }
+
+        private void OnDisable()
+        {
+            this.timeSO.TimeTicked -= this.TimeTickedEventHandler;
+        }
+
+        private void TimeTickedEventHandler(object sender, TimeTickedEventArgs e)
+        {
+            int percentage = (int) (e.ToT * 100.0f);
+            this.textGUI.text = percentage + "%";
+        }
     }
 }
